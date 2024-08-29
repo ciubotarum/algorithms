a365d924299158aa23da5651390bf12939de5e85
ciubotarum <ciubotarum@outlook.com>
2024-08-29 16:38:12 +0300

    Create threads using extends Thread

---
+++ src/multithreading/basicMultithreading/ExtendsThreadExample.java
@@ -0,0 +1,30 @@
+package multithreading.basicMultithreading;
+
+public class ExtendsThreadExample {
+    public static void main(String[] args) {
+        // Executing threads by system availability
+        Thread one = new Thread1();
+        Thread two = new Thread2();
+
+        one.start();
+        two.start();
+    }
+}
+
+class Thread1 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Thread1: " + i);
+        }
+    }
+}
+
+class Thread2 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Thread2: " + i);
+        }
+    }
+}
