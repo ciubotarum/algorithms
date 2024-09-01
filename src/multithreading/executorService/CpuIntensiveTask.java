90e6273e6a01465ad0273a3a48c96dc15fc4de81
ciubotarum <ciubotarum@outlook.com>
2024-09-01 14:50:17 +0300

    Cpu intensive task

---
+++ src/multithreading/executorService/CpuIntensiveTask.java
@@ -0,0 +1,26 @@
+package multithreading.executorService;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CpuIntensiveTask {
+    public static void main(String[] args) {
+        // number of the cores
+        int cores = Runtime.getRuntime().availableProcessors();
+        ExecutorService service = Executors.newFixedThreadPool(cores);
+        System.out.println("Created thread pool with : " + cores + " cores");
+
+        for (int i = 0; i < 20; i++) {
+            service.execute(new CpuTask());
+        }
+    }
+}
+
+class CpuTask implements Runnable {
+
+
+    @Override
+    public void run() {
+        System.out.println("Some CPU intensive task bing done by : " + Thread.currentThread().getName());
+    }
+}
