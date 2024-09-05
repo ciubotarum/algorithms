efde95468b43f03ec6870d49cf9dc193313db443
ciubotarum <ciubotarum@outlook.com>
2024-09-05 15:17:23 +0300

    ReentrantLock causing Dead Lock

---
+++ src/multithreading/otherConcepts/DeadLockDemo.java
@@ -0,0 +1,68 @@
+package multithreading.otherConcepts;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DeadLockDemo {
+    private final Lock lockA = new ReentrantLock(true);
+    private final Lock lockB = new ReentrantLock(true);
+
+    public void workerOne() {
+        lockA.lock();
+        System.out.println("Worker One acquired lockA");
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        lockB.lock();
+        System.out.println("Worker One acquired lockB");
+        lockA.unlock();
+        lockB.unlock();
+    }
+
+    public void workerTwo() {
+        lockB.lock();
+        System.out.println("Worker One acquired lockB");
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        lockA.lock();
+        System.out.println("Worker One acquired lockA");
+        lockA.unlock();
+        lockB.unlock();
+    }
+
+    public static void main(String[] args) {
+        DeadLockDemo demo = new DeadLockDemo();
+        new Thread(demo::workerOne, "Worker One").start();
+        new Thread(demo::workerTwo, "Worker Two").start();
+
+        // Approach to find DeadLocks
+        // Try to understand if there are any threads in deadlock
+        new Thread(() -> {
+            ThreadMXBean mxBean = ManagementFactory.getThreadMXBean();
+            while (true) {
+                long[] threadIds = mxBean.findDeadlockedThreads();   // more information about deadlock
+                if (threadIds != null) {
+                    System.out.println("Deadlock detected");
+                    ThreadInfo[] threadInfo = mxBean.getThreadInfo(threadIds);
+                    for (long threadId : threadIds) {
+                        System.out.println("Thread with ID " + threadId + " is in Deadlock");
+                    }
+                    break;
+                }
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+    }
+}
